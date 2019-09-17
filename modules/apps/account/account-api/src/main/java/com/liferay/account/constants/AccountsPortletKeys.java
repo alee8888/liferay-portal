f99263eb70ed28eada62cd8b3676ec960f6c2eec
alee8888 <albert.lee@liferay.com>
2019-09-17 12:13:35 -0700

    LPS-101794 Setup Account Users Portlet

--- modules/apps/account/account-api/src/main/java/com/liferay/account/constants/AccountsPortletKeys.java
+++ modules/apps/account/account-api/src/main/java/com/liferay/account/constants/AccountsPortletKeys.java
@@ -19,6 +19,9 @@ package com.liferay.account.constants;
  */
 public class AccountsPortletKeys {
 
+	public static final String ACCOUNT_USERS_ADMIN =
+		"com_liferay_account_users_admin_web_internal_portlet_AccountUsersAdminPortlet";
+
 	public static final String ACCOUNTS_ADMIN =
 		"com_liferay_account_admin_web_internal_portlet_AccountsAdminPortlet";
 
